14d3567a559cf0524d483aa2235162d3fb89dff7
tamayo2 <juan.tamayo3@utp.edu.co>
2024-06-10 17:45:41 -0500

    arreglos

--- React/clonFreeCode/src/App.js
+++ React/clonFreeCode/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='App'>
       <div className='contenedor-principal'>
-        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <h1>Esto es lo que dicen los alumnos de freeCodeCamp:</h1>
         <Testimonio
           nombre='Shawn Wang'
           pais='Singapur'
